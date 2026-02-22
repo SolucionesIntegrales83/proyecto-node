58f8bbdd0fb3dcd637a4933d52b14b740d639072
SolucionesIntegrales83 <multiusuario123456@gmail.com>
2026-02-22 12:41:22 +0100

    cuarto commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,5 @@
 import "@/styles/globals.css";
 
-export default function Home(){
-  return(
-    <main>
-      <h1>Portfolio en construción</h1>
-      <img src="https://concepto.de/wp-content/uploads/2019/05/leon-sabana-africa-e1559242836802.jpg"></img>
-    </main>
-  )
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
