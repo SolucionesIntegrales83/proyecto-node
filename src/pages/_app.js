14c16089a9cfb5acbaeee8837daf24e8493ae207
SolucionesIntegrales83 <multiusuario123456@gmail.com>
2026-02-22 12:21:52 +0100

    Segundo commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function Home(){
+  return(
+    <main>
+      <h1>Portfolio en construción</h1>
+    </main>
+  )
 }
