eaca1857310db2334c7aea71440c6bece7bfd44e
SolucionesIntegrales83 <multiusuario123456@gmail.com>
2026-02-22 12:30:10 +0100

    Tercer commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,7 @@ export default function Home(){
   return(
     <main>
       <h1>Portfolio en construción</h1>
+      <img src="https://concepto.de/wp-content/uploads/2019/05/leon-sabana-africa-e1559242836802.jpg"></img>
     </main>
   )
 }
